bd2c20ba1f32ab93828ce84ec5ff96e3532a26b3
Motasim Hussain <motasimhussain@gmail.com>
2013-06-30 14:53:52 +0500

    shit happens

--- WindowsFormsApplication1/dailyRep.cs
+++ WindowsFormsApplication1/dailyRep.cs
@@ -38,5 +38,10 @@ namespace WindowsFormsApplication1
             }
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
